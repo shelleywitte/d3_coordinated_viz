4ac2e1fcc0f06c5f27855a8d94cd792720945a9d
shelleywitte <switte@wisc.edu>
2016-03-16 16:19:02 -0500

    d3.js + topojson.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+window.onload = setMap();
 
+function setMap() {
+
+    var q = d3_queue.queue();
+
+    q
+        .defer(d3.csv, "data/8th_grade_science_achievement_gap.csv")
+        .defer(d3.json, "data/choropleth_map.topojson")
+        .await(callback);
+
+    function callback(error, csvData, unitedStates) {
+        var us = topojson.feature(unitedStates, unitedStates.objects.choropleth_map).features;
+
+        console.log(unitedStates);
+    };
+};
