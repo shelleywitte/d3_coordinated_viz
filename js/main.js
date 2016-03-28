c1dd0c45b8433ecbd0bb19cff540e174f866460c
shelleywitte <switte@wisc.edu>
2016-03-27 21:38:39 -0500

    projection + drawn map
    map not showing up

--- js/main.js
+++ js/main.js
@@ -1,17 +1,42 @@
 window.onload = setMap();
 
 function setMap() {
+    var width = 960,
+        height = 460;
 
-    var q = d3_queue.queue();
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
 
-    q
+    var projection = d3.geo.albers()
+        .center([5, 37])
+        .rotate([-100, 0, 0])
+        .parallels([30, 46])
+        .scale(600)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geo.path()
+        .projection(projection);
+
+    d3_queue.queue()
         .defer(d3.csv, "data/8th_grade_science_achievement_gap.csv")
-        .defer(d3.json, "data/choropleth_map.topojson")
+        .defer(d3.json, "data/US_shapefile.topojson")
         .await(callback);
 
     function callback(error, csvData, unitedStates) {
-        var us = topojson.feature(unitedStates, unitedStates.objects.choropleth_map).features;
+        var us = topojson.feature(unitedStates, unitedStates.objects.US_shapefile).features;
 
-        console.log(unitedStates);
+        console.log(us);
+        
+        var states = map.selectAll(".states")
+            .data(us)
+            .enter()
+            .append("path")
+            .attr("class", function(d) {
+                return "states " + d.properties.name;
+            })
+            .attr("d", path);
     };
 };
